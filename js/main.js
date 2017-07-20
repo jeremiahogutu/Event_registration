c185ba188f43301d076e007413b9bf65d3d0d693
Jeremiahogutu <jeremiahogutu@protonmail.com>
2017-07-20 03:00:03 -0400

    Added sidebar navigation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-function isValidEmail(emailAddress) {
-            var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+$(document).ready(function(){
+	$('.sidebarBtn').click(function(){
+		$('.sidebar').toggleClass('active');
+		$('.sidebarBtn').toggleClass('toggle');
+	})
+})
 
-            return pattern.test(emailAddress);
 
-}
+
+// function isValidEmail(emailAddress) {
+//             var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+//             return pattern.test(emailAddress);
+
+// }
