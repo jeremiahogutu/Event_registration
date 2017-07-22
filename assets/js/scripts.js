f794fa67fe18c7044c371edecb807a403118ac1c
Jeremiah Ogutu <jeremiahogutu1@gmail.com>
2017-07-22 18:36:46 -0400

    Added toggler to error message

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,7 @@
-
-(function ($) {
+(function($) {
     'use strict';
 
-    jQuery(document).ready(function () {
+    jQuery(document).ready(function() {
 
 
         /*
@@ -13,16 +12,16 @@
 
 
         $(window).on('load', function() {
-          $('body').addClass('loaded');
+            $('body').addClass('loaded');
         });
 
 
-        $(document).ready(function(){
-    $('.sidebarBtn').click(function(){
-        $('.sidebar').toggleClass('active');
-        $('.sidebarBtn').toggleClass('toggle');
+        $(document).ready(function() {
+            $('.sidebarBtn').click(function() {
+                $('.sidebar').toggleClass('active');
+                $('.sidebarBtn').toggleClass('toggle');
+            })
         })
-    })
 
 
 
@@ -32,7 +31,7 @@
          * ----------------------------------------------------------------------------------------
          */
 
-        $('a.smooth-scroll').on("click", function (e) {
+        $('a.smooth-scroll').on("click", function(e) {
             var anchor = $(this);
             $('html, body').stop().animate({
                 scrollTop: $(anchor.attr('href')).offset().top - 50
@@ -46,14 +45,14 @@
          *  SCROLL TO UP JS
          * ----------------------------------------------------------------------------------------
          */
-        $(window).on("scroll", function () {
+        $(window).on("scroll", function() {
             if ($(this).scrollTop() > 250) {
                 $('.scrollup').fadeIn();
             } else {
                 $('.scrollup').fadeOut();
             }
         });
-        $('.scrollup').on("click", function () {
+        $('.scrollup').on("click", function() {
             $("html, body").animate({
                 scrollTop: 0
             }, 800);
@@ -67,66 +66,56 @@
          */
         new WOW().init();
 
-
         /*
          * ----------------------------------------------------------------------------------------
-         *  Form validation
+         *  Postal code field toggler
          * ----------------------------------------------------------------------------------------
          */
 
-// Postal codes do not include the letters D, F, I, O, Q or U, and the first position also does not make use of the letters W or Z.
-    function checkPostal(postal) {
+        $(document).ready(function() {
+            postalToggler(); // sets the visibility based on the selected values
 
-    //Regex code to validate the postal code
-    var regex = new RegExp(/^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i);
-    //Gets the value of the postal code entered by the user
-    var postal = document.getElementById("postal").value
-    if (regex.test(postal.value))
-        return true;//test passes if postal code is valid
-    else
-        //test fails if postal code is invalid
-        alert("please enter valid postal code") 
-        return false;
-    }
-
-
-        $(document).ready(function () {
-            toggler(); // sets the visibility based on the selected values
-
-        //calls toggler function every time the selection value of the field changes
-        $("#country").change(function () {
-            toggler();
+            //calls toggler function every time the selection value of the field changes
+            $("#country").change(function() {
+                postalToggler();
+            });
         });
-    });
         // this toggles the visibility of postal code field
-        function toggler() {
+        function postalToggler() {
             if ($("#country").val() === "canada")
-        $("#postal").show();
+                $("#postal, #demo").show();
+
             else
-                $("#postal").hide();
+                $("#postal, #demo").hide();
         }
 
-
-
-});
+    });
 
 })(jQuery);
+    
+    /*
+        * ----------------------------------------------------------------------------------------
+        *  Validate Canadian Postal Code
+        * ----------------------------------------------------------------------------------------
+        */
 
-function validate(){
-        
+        function validate() {
 
-        var newVal = document.getElementById("postal").value;
-
-        var reg = /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i;
-
-        if (reg.test(postal.value)== false) {
-            document.getElementById("demo").style.color = "red";
-                document.getElementById("demo").innerHTML ="Invalid postal Code: "+ newVal;
-                return false;
-            } else{
-            document.getElementById("demo").style.color = "DarkGreen";      
-            document.getElementById("demo").innerHTML ="Valid Postal Code: "+newVal;
+            // Postal codes do not include the letters D, F, I, O, Q or U, and the first position also does not make use of the letters W or Z.
+            var newVal = document.getElementById("postal").value;
+            //Regex code to validate the postal code
+            var reg = /^[ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ]( )?\d[ABCEGHJKLMNPRSTVWXYZ]\d$/i;
+            //Gets the value of the postal code entered by the user
+            if (reg.test(postal.value) == false) {
+                document.getElementById("demo").style.color = "red";
+                document.getElementById("demo").innerHTML = "Invalid postal Code: " + newVal;
+                return false; //test fails if postal code is invalid
+            } else {
+                //test fails if postal code is valid
+                document.getElementById("demo").style.color = "DarkGreen";
+                document.getElementById("demo").innerHTML = "Valid Postal Code: " + newVal;
             }
 
             return true;
         }
+    
